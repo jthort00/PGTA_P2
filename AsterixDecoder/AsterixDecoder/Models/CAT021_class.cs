1997ea7cf4484f22ba2799d6e70e2b7358c58064
fedepompeo <federico.pompeo@estudiantat.upc.edu>
2025-10-06 00:16:40 +0200

    feat: Introduction of ASTERIX CAT021 decoder

---
+++ AsterixDecoder/AsterixDecoder/Models/CAT021_class.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsterixDecoder.Models
+{
+    /// <summary>
+    /// Decodificador ASTERIX CAT021 
+    /// </summary>
+    public class Cat048Decoder
+    {
+        
+    }
